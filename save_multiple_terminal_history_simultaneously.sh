5f4d4cdd457ee4cb4bc56af8dcfcaaa29d3d6591
Abhishek Ajmani <a.ajmani@alumni.iitg.ac.in>
2025-08-22 08:39:02 +0530

    Update save_unlimited_history_commands_from_multiple_terminal_simultaneously.sh

--- save_multiple_terminal_history_simultaneously.sh
+++ save_multiple_terminal_history_simultaneously.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 shopt -s histappend
 PROMPT_COMMAND="history -a; history -n"
+sed -i 's/HISTSIZE=*/export HISTSIZE=-1/g' ~/.bashrc
+sed -i 's/HISTFILESIZE=*/export HISTFILESIZE=-1/g' ~/.bashrc
 source ~/.bashrc
